cafe8cd0db4d39c7c1f605150afbd73250ef1a87
Manmeet Saluja <manmeet.saluja@u.nus.edu>
2016-03-25 22:04:35 +0800

    solved difference of squares problem

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,8 +1,26 @@
 class Squares
+  VERSION=2
   def initialize(num)
     @num = num
   end
 
-  def square_of_sum num
+  def square_of_sum
+    sum = 0
+    for i in 0..@num
+      sum += i
+    end
+    sum**2
+  end
+
+  def sum_of_squares
+    sum = 0
+    for i in 0..@num
+      sum += (i**2)
+    end
+    sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
   end
 end
