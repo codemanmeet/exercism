e9cef29d4b3a8880a289faf3e3f118f57df2b20f
Manmeet Saluja <manmeet.saluja@u.nus.edu>
2016-05-15 16:16:58 +0530

    added scala bob solution

---
+++ scala/bob/src/main/scala/Bob.scala
@@ -0,0 +1,25 @@
+class Bob {
+  val WHATEVER = "Whatever."
+  val SURE = "Sure."
+  val FINE = "Fine. Be that way!"
+  val CHILL_OUT = "Whoa, chill out!"
+  def hey(str: String): String = {
+    if(areAllLettersCapital(str)){
+      return CHILL_OUT
+    }
+    if(str.trim == ""){
+      return FINE
+    }
+    if(str.takeRight(1) == "?"){
+      return SURE
+    }
+    return WHATEVER
+
+  }
+
+  def areAllLettersCapital(str: String): Boolean = {
+    val allLetters = ('a' to 'z') ++ ('A' to 'Z')
+    val strWithAlpha = str.filter(allLetters.contains)
+    return (strWithAlpha.forall(_.isUpper) && strWithAlpha.length > 0)
+  }
+}
