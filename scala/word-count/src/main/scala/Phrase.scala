6ba01715f424e10125682e8660120d2287d8671a
Manmeet Saluja <manmeet.saluja@u.nus.edu>
2016-05-22 01:20:19 +0800

    scala word count problem solved.

--- scala/word-count/src/main/scala/Phrase.scala
+++ scala/word-count/src/main/scala/Phrase.scala
@@ -2,5 +2,14 @@
   * Created by manmeetsinghsaluja on 18/5/16.
   */
 class Phrase(str: String) {
-
+  def wordCount(): Map[String, Integer] ={
+    val arr = str.split("[^\\w']").filter(_.length> 0).map(_.toLowerCase)
+    var m:Map[String,Integer] = Map()
+    for(c <- arr){
+      if(!m.contains(c)){
+        m += (c -> arr.count(_ == c))
+      }
+    }
+    return m
+  }
 }
