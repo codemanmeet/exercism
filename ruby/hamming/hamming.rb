50dac08da8efb97166fda99f6b3773bca89c7244
Manmeet Saluja <manmeet.saluja@u.nus.edu>
2016-08-25 00:57:01 +0800

    refactoring roman numerals and hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,9 @@
 class Hamming
   VERSION=1
   def self.compute(a, b)
-    if a.length != b.length
-      raise ArgumentError
-    end
+    raise ArgumentError if a.length != b.length
     diff = 0
-    (0..a.length-1).each do |i|
+    (0...a.length).each do |i|
       diff += 1 unless a[i] == b[i]
     end
     diff
--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -6,7 +6,6 @@ class Fixnum
     'XC' => 90,  'L' => 50,  'XL' => 40,  'X' => 10,
     'IX' => 9,   'V' => 5,   'IV' => 4,   'I' => 1
   }.freeze
-
   def to_roman
     num = self
     ROMAN_NUMS.map do |ltr, val|
